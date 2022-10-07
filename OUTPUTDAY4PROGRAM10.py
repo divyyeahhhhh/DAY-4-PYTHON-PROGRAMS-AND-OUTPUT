4ec5c9b6b72e0cebf924b74033a626a40e959ce0
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-07 12:06:33 +0530

    Add files via upload

---
+++ OUTPUTDAY4PROGRAM10.py
@@ -0,0 +1,28 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM10.py 
+enter first word:GREAT
+enter second word:RGEAT
+TRUE
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM10.py 
+enter first word:ABCDE
+enter second word:CAEBD
+FALSE
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM10.py 
+enter first word:A
+enter second word:A
+TRUE
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM10.py 
+enter first word:AB
+enter second word:AD
+FALSE
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM10.py 
+enter first word:10
+enter second word:-5
+FALSE
+>>> 
