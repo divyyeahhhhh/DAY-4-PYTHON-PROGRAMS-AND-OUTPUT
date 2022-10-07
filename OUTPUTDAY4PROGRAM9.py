96b88ec7a600d90abbaf3ac9bdd04541cabb01e5
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-07 12:03:54 +0530

    Add files via upload

---
+++ OUTPUTDAY4PROGRAM9.py
@@ -0,0 +1,28 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM9.py 
+ENTER THE MONTH: JULY
+ENTER THE DATE: 29
+Season is SPRING
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM9.py 
+ENTER THE MONTH: SEPTEMBER
+ENTER THE DATE: 5
+Season is FALL
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM9.py 
+ENTER THE MONTH: DECEMBER
+ENTER THE DATE: 30
+Season is WINTER
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM9.py 
+ENTER THE MONTH: MARCH
+ENTER THE DATE: 12
+Season is SUMMER
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM9.py 
+ENTER THE MONTH: JUNE
+ENTER THE DATE: 27
+Season is FALL
+>>> 
