e4a377544668d48533800569b15303b49405b5b9
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-07 11:49:16 +0530

    Add files via upload

---
+++ OUTPUTDAY4PROGRAM4.py
@@ -0,0 +1,23 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM4.py 
+Enter the phrase: "A man,a plan,a canal: panama"
+True
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM4.py 
+Enter the phrase: "race a car"
+False
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM4.py 
+Enter the phrase: " "
+True
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM4.py 
+Enter the phrase: madam
+True
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM4.py 
+Enter the phrase: "honest"
+False
+>>> 
