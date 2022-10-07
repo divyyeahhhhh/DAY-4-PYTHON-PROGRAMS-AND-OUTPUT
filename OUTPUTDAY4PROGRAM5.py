78e398d4ae74b3791b79bd3f9b264829699c77d5
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-07 11:52:04 +0530

    Add files via upload

---
+++ OUTPUTDAY4PROGRAM5.py
@@ -0,0 +1,66 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM5.py 
+Enter the no. of elements: 11
+Enter the value: 
+1
+3
+5
+8
+9
+2
+6
+7
+6
+8
+9
+3
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM5.py 
+Enter the no. of elements: 11
+Enter the value: 
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+10
+>>> 1
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM5.py 
+Enter the no. of elements: 5
+Enter the value: 
+2
+3
+1
+1
+4
+2
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM5.py 
+Enter the no. of elements: 6
+Enter the value: 
+1
+3
+6
+1
+0
+9
+3
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM5.py 
+Enter the no. of elements: 5
+Enter the value: 
+2
+3
+0
+1
+4
+2
+>>> 
