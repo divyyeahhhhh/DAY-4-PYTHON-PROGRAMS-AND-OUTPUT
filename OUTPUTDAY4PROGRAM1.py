dd0f7d29d6105de3e2bcfe0f3e9787b5e92fd759
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-07 11:40:43 +0530

    Add files via upload

---
+++ OUTPUTDAY4PROGRAM1.py
@@ -0,0 +1,28 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM1.py 
+Enter the value: 3
+Sequence: 
+[1, 2, 'Fizz']
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM1.py 
+Enter the value: 5
+Sequence: 
+[1, 2, 'Fizz', 4, 'Buzz']
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM1.py 
+Enter the value: 15
+Sequence: 
+[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM1.py 
+Enter the value: 10
+Sequence: 
+[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\DAY4PROGRAM1.py 
+Enter the value: 20
+Sequence: 
+[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz']
+>>> 
